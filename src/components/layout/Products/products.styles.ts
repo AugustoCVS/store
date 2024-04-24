8cbbcda1f4ea30f71efb8b818721ab46f1b3bab5
AugustoCVS <guty.vertolis@gmail.com>
2024-04-24 18:23:59 -0300

    feat: add responsivity

--- src/components/layout/Products/products.styles.ts
+++ src/components/layout/Products/products.styles.ts
@@ -7,11 +7,23 @@ export const Container = styled.section`
   align-items: center;
   justify-content: center;
   padding: 5rem 0;
-`
+`;
 
 export const ProductWrapper = styled.div`
   max-width: 58.625rem;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: 1fr;
   gap: 1.5rem;
-`
+
+  @media (min-width: 500px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 770px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 1000px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
