c0223bcd6a61ba24db7385f00a7ee6d8599f1d71
AugustoCVS <guty.vertolis@gmail.com>
2024-04-24 19:34:12 -0300

    feat: render skeleton if is loading

--- src/components/layout/Products/products.component.tsx
+++ src/components/layout/Products/products.component.tsx
@@ -5,9 +5,30 @@ import React from "react";
 import * as S from "./products.styles";
 import { useProducts } from "./products.hook";
 import { ProductCard } from "./components/ProductCard/product-card.component";
+import { SkeletonComponent } from "../../common/Skeleton/skeleton.component";
 
 export const Products: React.FC = () => {
   const { states } = useProducts();
+
+  if (states.isLoading) {
+    return (
+      <S.Container>
+        <S.ProductWrapper>
+          {Array.from({ length: 8 }).map((_, index) => (
+            <SkeletonComponent
+              key={index}
+              height={284}
+              width={218}
+              baseColor="#2C2C2C"
+              borderRadius={8}
+              highlightColor="#373737"
+              testId="product-skeleton"
+            />
+          ))}
+        </S.ProductWrapper>
+      </S.Container>
+    );
+  }
 
   return (
     <S.Container>
