0298f85a1e8d4400e21e79ddc671147d0d787882
AugustoCVS <guty.vertolis@gmail.com>
2024-04-24 12:15:24 -0300

    feat: create card types

---
+++ src/components/common/Card/card.types.ts
@@ -0,0 +1,15 @@
+export type RootProps = {
+  children: React.ReactNode;
+};
+
+export type ImageProps = {
+  url: string;
+};
+
+export type TextProps = {
+  text: string;
+}
+
+export type ButtonProps = {
+  onClick: () => void;
+}
