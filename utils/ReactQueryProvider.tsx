62ba3d280fb16228c0c9ef85ca373199887dd23a
AugustoCVS <guty.vertolis@gmail.com>
2024-04-24 01:35:59 -0300

    build: initial commit

---
+++ utils/ReactQueryProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const ReactQueryProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
+};
