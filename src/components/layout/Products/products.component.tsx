9c4a53c88ad99b2a85b91b5145cc4c097a4e9c2c
AugustoCVS <guty.vertolis@gmail.com>
2024-04-24 17:43:18 -0300

    feat: create the products component

--- src/components/layout/Products/products.component.tsx
+++ src/components/layout/Products/products.component.tsx
@@ -1,5 +1,30 @@
+"use client";
+
 import React from "react";
 
+import * as S from "./products.styles";
+import { useProducts } from "./products.hook";
+import { ProductCard } from "./components/ProductCard/product-card.component";
+
 export const Products: React.FC = () => {
-  return <>pizza</>;
+  const { states } = useProducts();
+
+  return (
+    <S.Container>
+      <S.ProductWrapper>
+        {states.data?.products.map((product) => {
+          return (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              photo={product.photo}
+              price={product.price}
+              description={product.description}
+            />
+          );
+        })}
+      </S.ProductWrapper>
+    </S.Container>
+  );
 };
