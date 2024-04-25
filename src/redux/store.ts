afbbcb5d210a12192bf50af00f471d294267c6da
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 15:04:14 -0300

    refactor: change store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productsReducer } from './Slices/products.slice'
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
     products: productsReducer
   }
 })
+
