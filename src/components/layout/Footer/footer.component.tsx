f4afdbc481a34407c0dfdfed856ffd63499aaad4
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 18:12:00 -0300

    feat: create footer component

---
+++ src/components/layout/Footer/footer.component.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+import * as S from "./footer.styles";
+
+export const Footer: React.FC = () => {
+  return (
+    <S.FooterContainer>
+      <S.FooterText>MKS sistemas © Todos os direitos reservados</S.FooterText>
+    </S.FooterContainer>
+  );
+};
