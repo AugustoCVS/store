0eaa3eea5b4909814b25d75d7e3f58657708e094
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 15:04:33 -0300

    feat: create productsSlice

--- src/redux/Slices/products.slice.ts
+++ src/redux/Slices/products.slice.ts
@@ -1,18 +1,36 @@
-
 import { ProductProps } from "@/src/@types/product";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
+const initialState: ProductProps[] = [];
+
 export const productsSlice = createSlice({
   name: "products",
-  initialState: {
-    products: [] as ProductProps[], 
-  },
+  initialState,
   reducers: {
-    setProducts: (state, action: PayloadAction<ProductProps[]>) => {
-      state.products = action.payload;
+    addProduct: (state, action: PayloadAction<ProductProps>) => {
+      const product = state.find((p) => p.id === action.payload.id);
+      if (product) {
+        product.quantity && product.quantity++;
+        return;
+      }
+      state.push({ ...action.payload, quantity: 1 });
+    },
+
+    increaseProductQuantity: (state, action: PayloadAction<ProductProps>) => {
+      const product = state.find((p) => p.id === action.payload.id);
+      if (product) {
+        product.quantity && product.quantity++;
+      }
+    },
+
+    decreaseProductQuantity: (state, action: PayloadAction<ProductProps>) => {
+      const product = state.find((p) => p.id === action.payload.id);
+      if (product) {
+        product.quantity && product.quantity--;
+      }
     },
   },
 });
 
-export const { setProducts } = productsSlice.actions;
+export const { addProduct } = productsSlice.actions;
 export const productsReducer = productsSlice.reducer;
