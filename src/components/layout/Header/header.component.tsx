b727c28fdc6e156257bb90d5e832ba84d9d51cee
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 11:17:30 -0300

    feat: implement the hook

--- src/components/layout/Header/header.component.tsx
+++ src/components/layout/Header/header.component.tsx
@@ -6,16 +6,24 @@ import * as S from "./header.styles";
 import { Title } from "./components/Title/title.component";
 import { CartButton } from "./components/CartButton/cart-button.component";
 import { Cart } from "@/src/components/layout/Cart/cart.component";
+import { useHeader } from "./header.hook";
+import { AnimatePresence } from "framer-motion";
 
 export const Header: React.FC = () => {
+  const { states, actions } = useHeader();
+
   return (
     <>
       <S.Container>
         <Title />
-        <CartButton />
+        <CartButton onClick={actions.handleToggleCart} />
       </S.Container>
 
-      <Cart />
+      <AnimatePresence mode="wait">
+        {states.isCartOpen && (
+          <Cart handleCloseTheCart={actions.handleToggleCart} />
+        )}
+      </AnimatePresence>
     </>
   );
 };
