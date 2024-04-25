16e704cb007c2f811f0b55f739995a16967af827
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 11:18:08 -0300

    feat: add handleCloseTheCart

--- src/components/layout/Cart/components/Header/header.component.tsx
+++ src/components/layout/Cart/components/Header/header.component.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { X } from "lucide-react";
 
 import * as S from "./header.styles";
+import { HeaderProps } from "./header.types";
 
-export const Header: React.FC = () => {
+export const Header: React.FC<HeaderProps> = ({ handleCloseTheCart }) => {
   return (
     <S.Container>
       <S.Title>
         Carrinho <br /> de compras
       </S.Title>
-      <S.Button>
+      <S.Button onClick={handleCloseTheCart}>
         <X color="white" width={25} height={25} />
       </S.Button>
     </S.Container>
