75aee0e07519bcca632d16bcb73acf688e208b27
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 11:16:57 -0300

    feat: add onClick on CartButton

--- src/components/layout/Header/components/CartButton/cart-button.component.tsx
+++ src/components/layout/Header/components/CartButton/cart-button.component.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { ShoppingCart } from "lucide-react";
 
 import * as S from "./cart-button.styles";
+import { CartButtonProps } from "./cart-button.types";
 
-export const CartButton: React.FC = () => {
+export const CartButton: React.FC<CartButtonProps> = ({ onClick }) => {
   return (
-    <S.Wrapper>
+    <S.Wrapper onClick={onClick}>
       <ShoppingCart size={18} color="black" />
       <S.CartText>0</S.CartText>
     </S.Wrapper>
