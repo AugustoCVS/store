350483264ab5f55758402078bf9ddd993f754458
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 16:20:04 -0300

    feat: create handleSendToast

--- src/components/layout/Products/products.hook.ts
+++ src/components/layout/Products/products.hook.ts
@@ -1,7 +1,10 @@
+import { ToastProps } from "@/src/@types/toast";
 import { ProductRequest } from "@/src/services/interfaces/Product/product.type";
 import { ProductService } from "@/src/services/products";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { toast } from "react-toastify";
+import { ERROR_MESSAGE } from "./products.constants";
 
 export const useProducts = () => {
   const [reqParams, setReqParams] = useState<ProductRequest>({
@@ -10,11 +13,18 @@ export const useProducts = () => {
     sortBy: "name",
     orderBy: "DESC",
   });
+
+  const handleSendToast = ({message, type}: ToastProps) => {
+    return type === "error" ? toast.error(message) : toast.success(message);
+  }
   
   const { data, isLoading } = useQuery({
     queryKey: ["products"],
     queryFn: () => ProductService.getProducts({
       data: reqParams,
+    }).catch((err: Error) => {
+      console.error(err);
+      handleSendToast({ message: ERROR_MESSAGE, type: "error" });
     }),
     refetchOnWindowFocus: false,
     retry: false,
