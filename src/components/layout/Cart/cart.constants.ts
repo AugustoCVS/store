872a2f4db75d0e619997fcea1c46ceed8206c62a
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 11:17:59 -0300

    feat: create cart constants

--- src/components/layout/Cart/cart.constants.ts
+++ src/components/layout/Cart/cart.constants.ts
@@ -1,16 +1,12 @@
 export const menuSlide = {
-  initial: {x: "calc(100% + 100px)"},
+  initial: {
+    x: "100%",
+  },
   enter: {x: "0", transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1]}},
-  exit: {x: "calc(100% + 100px)", transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1]}}
-}
-
-export const slide = {
-  initial: {x: 80},
-  enter: (i: any) => ({x: 0, transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i}}),
-  exit: (i: any) => ({x: 80, transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.05 * i}})
-}
+  exit: {x: "100%", transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1]}},
+};
 
 export const scale = {
-  open: {scale: 1, transition: {duration: 0.3}},
-  closed: {scale: 0, transition: {duration: 0.4}}
-}
+  open: { scale: 1, transition: { duration: 0.3 } },
+  closed: { scale: 0, transition: { duration: 0.4 } },
+};
