385deb33acab686b40cca54f4fa64addd715bb92
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 11:26:06 -0300

    feat: remove scale

--- src/components/layout/Cart/cart.constants.ts
+++ src/components/layout/Cart/cart.constants.ts
@@ -5,8 +5,3 @@ export const menuSlide = {
   enter: {x: "0%", transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1]}},
   exit: {x: "calc(100% + 100px)", transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1]}},
 };
-
-export const scale = {
-  open: { scale: 1, transition: { duration: 0.3 } },
-  closed: { scale: 0, transition: { duration: 0.4 } },
-};
