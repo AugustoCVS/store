777deaea1e06169708cd797d0a79a8fc2b92ac74
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 10:50:38 -0300

    feat: create the curve component

---
+++ src/components/layout/Cart/components/Curve/curve.styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SvgCurveContainer = styled.svg`
+  position: absolute;
+  top: 0;
+  left: -99px;
+  width: 100px;
+  height: 100%;
+  fill: rgb(41, 41, 41);
+  stroke: none;
+`;
