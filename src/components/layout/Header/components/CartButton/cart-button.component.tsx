8193b9ad1c86865f2f5763006ae24fc4bef2c19f
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 15:44:15 -0300

    feat: create useCartButton

--- src/components/layout/Header/components/CartButton/cart-button.component.tsx
+++ src/components/layout/Header/components/CartButton/cart-button.component.tsx
@@ -3,12 +3,15 @@ import { ShoppingCart } from "lucide-react";
 
 import * as S from "./cart-button.styles";
 import { CartButtonProps } from "./cart-button.types";
+import { useCartButton } from "./cart-button.hook";
 
 export const CartButton: React.FC<CartButtonProps> = ({ onClick }) => {
+  const { actions } = useCartButton();
+
   return (
     <S.Wrapper onClick={onClick}>
       <ShoppingCart size={18} color="black" />
-      <S.CartText>0</S.CartText>
+      <S.CartText>{actions.handleGetTotal()}</S.CartText>
     </S.Wrapper>
   );
 };
