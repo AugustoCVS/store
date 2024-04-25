fa9a418431010329821bf58f7fc334c3b9d89d79
AugustoCVS <guty.vertolis@gmail.com>
2024-04-25 15:05:13 -0300

    refactor: update import on redux provider

--- src/Providers/ReduxProvider.tsx
+++ src/Providers/ReduxProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Provider } from "react-redux";
-import store from "@/src/redux/store";
+import { store } from "@/src/redux/store";
 
 export const ReduxProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
