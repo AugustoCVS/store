ac1f56d950610527058d870f0a6cef439d10bbd2
AugustoCVS <guty.vertolis@gmail.com>
2024-04-26 12:56:18 -0300

    feat: add data-testid

--- src/components/layout/Footer/footer.component.tsx
+++ src/components/layout/Footer/footer.component.tsx
@@ -3,11 +3,14 @@
 import React from "react";
 
 import * as S from "./footer.styles";
+import { TESTS_IDS } from "./footer.constants";
 
 export const Footer: React.FC = () => {
   return (
-    <S.FooterContainer>
-      <S.FooterText>MKS sistemas © Todos os direitos reservados</S.FooterText>
+    <S.FooterContainer data-testid={TESTS_IDS.CONTAINER}>
+      <S.FooterText data-testid={TESTS_IDS.TEXT}>
+        MKS sistemas © Todos os direitos reservados
+      </S.FooterText>
     </S.FooterContainer>
   );
 };
