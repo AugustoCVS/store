68055a422b1ed05c2a3d4197bbe49373349adb1d
AugustoCVS <guty.vertolis@gmail.com>
2024-04-26 10:49:55 -0300

    build: install jest libs

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,2 @@
-import "@testing-library/jest-dom";
-import "jest-styled-components";
-import "@testing-library/jest-dom/extend-expect";
+import '@testing-library/jest-dom'
+import 'jest-styled-components'
