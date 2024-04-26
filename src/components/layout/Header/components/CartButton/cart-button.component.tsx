879f4abc5c7ce06c0890cf0a093af79f73b1c7b7
AugustoCVS <guty.vertolis@gmail.com>
2024-04-26 13:06:41 -0300

    feat: add data-testid

--- src/components/layout/Header/components/CartButton/cart-button.component.tsx
+++ src/components/layout/Header/components/CartButton/cart-button.component.tsx
@@ -4,14 +4,17 @@ import { ShoppingCart } from "lucide-react";
 import * as S from "./cart-button.styles";
 import { CartButtonProps } from "./cart-button.types";
 import { useCartButton } from "./cart-button.hook";
+import { TESTS_IDS } from "./cart-button.constants";
 
 export const CartButton: React.FC<CartButtonProps> = ({ onClick }) => {
   const { actions } = useCartButton();
 
   return (
-    <S.Wrapper onClick={onClick}>
+    <S.Wrapper onClick={onClick} data-testid={TESTS_IDS.CONTAINER}>
       <ShoppingCart size={18} color="black" />
-      <S.CartText>{actions.handleGetTotal()}</S.CartText>
+      <S.CartText data-testid={TESTS_IDS.CART_TEXT}>
+        {actions.handleGetTotal()}
+      </S.CartText>
     </S.Wrapper>
   );
 };
---
+++ src/components/layout/Header/components/CartButton/cart-button.constants.ts
@@ -0,0 +1,4 @@
+export const TESTS_IDS = {
+  CONTAINER: "cart-button-container",
+  CART_TEXT: "cart-button-text",
+}
