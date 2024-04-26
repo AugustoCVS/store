8436b0004f75a5966842a7fd6ef686c90a937006
AugustoCVS <guty.vertolis@gmail.com>
2024-04-26 13:40:32 -0300

    feat: add data-testid

---
+++ src/components/layout/Header/header.constants.ts
@@ -0,0 +1,7 @@
+export const TESTS_IDS = {
+  CONTAINER: "header-container",
+  TITLE: "title-container",
+  CART_BUTTON: "cart-button-container",
+  CART_CONTAINER: "cart-container",
+  HEADER_CART_BUTTON: "header-button",
+}
